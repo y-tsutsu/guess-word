7057ef62047d1e88832496c93124047aff58be6e
tsutsu <tsutsu0928@gmail.com>
2023-09-24 20:09:54 +0900

    画面のウィジェットを配置

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ mod gui;
 
 fn main() -> iced::Result {
     let mut settings = Settings::default();
-    settings.window.size = (400, 200);
+    settings.window.size = (400, 600);
     gui::GuessWord::run(settings)
 }
